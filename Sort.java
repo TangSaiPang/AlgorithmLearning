debed72c90600ef2850c87fc95154b40a7a1a5ea
TangSaiPang <89617575+TangSaiPang@users.noreply.github.com>
2022-09-28 20:42:21 +0800

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -2,7 +2,10 @@ public class Sort {
     private Sort(){
     }
     
+    /*The Method Bubble Sort*/
     public static int BubbleSort(int[] arr){
-        for (int i = 0;)
+        for (int i = 0; i < arr.length; i++){
+            
+        }
     }
 }
